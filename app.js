3c4ae068527fc1ccf8b5d45a50cb8e33044b7620
MOHAMMAD HANEEF S <shaikmohammadhaneef01@gmail.com>
2026-05-27 15:31:17 +0530

    Remove server listening logic
    Removed server listening code from app.js.

--- app.js
+++ app.js
@@ -528,11 +528,6 @@ function logout(req, res) {
   res.clearCookie("cookuname");
   res.redirect("/signin");
 }
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
 
 
 module.exports = app;
